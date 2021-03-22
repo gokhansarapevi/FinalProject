698d8c638141b91e67143898808938a274d36832
gokhansarapevi <gokhansarapevi@gmail.com>
2021-03-22 08:00:23 +0300

    WebAPI is added to the project.

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -24,6 +24,10 @@ namespace Business.Concrete
         public IDataResult<List<Product>> GetAll()
         {
             //Varsa iş kodlarını buraya yazarız. (yetkisi var mı vs tarzında if clauses.)
+            if (DateTime.Now.Hour == 1)
+            {
+                return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
+            }
             return new SuccessDataResult<List<Product>>(_productDal.GetAll(),Messages.ProductsListed);
         }
 
