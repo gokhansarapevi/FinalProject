a637a22c8871dd96c6590986bc287bcb69ae9be8
gokhansarapevi <gokhansarapevi@gmail.com>
2021-04-07 09:02:39 +0300

    CategoriesController is added. GetAllByCategory method is added to ProductsController.

---
+++ WebAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Entities.Concrete;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            Thread.Sleep(1000);
+            var result = _categoryService.GetAll();
+            if (result.Success==true)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int Id)
+        {
+            var result = _categoryService.GetById(Id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+    }
+}
